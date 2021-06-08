0d74a0f99ec5731a3eb4b0fa493bb2c1a8f8120d
Abdulrhman Ghnem <abdalrhman.ib1998@gmail.com>
2021-06-08 16:48:03 +0800

    added the trajectory function added the inverse function added the plot function

--- PlanarRobot3DOFSimulation.m
+++ PlanarRobot3DOFSimulation.m
@@ -1,13 +1,26 @@
-L(1) = Link([0 0 1 0]);
-L(2) = Link([0 0 1 0]);
-L(3) = Link([0 0 1 0]);
+clear all;
+clc;
+close all;
+
+L1=10; L2=10; L3=10;
+L(1) = Link([0 0 L1 0]);
+L(2) = Link([0 0 L2 0]);
+L(3) = Link([0 0 L3 0]);
 ThreeLink = SerialLink(L);
 ThreeLink.name = 'Planar3R';
-%ThreeLink.plot([pi/4 pi/4 pi/4]);
-ThreeLink.fkine([pi/4 pi/4 pi/4]);
-TL = [-1 0 0 0 ; 0 -1 0 1; 0 0 1 0; 0 0 0 1];
-Q0 = [0 -pi/2 pi/2];
-QF = ThreeLink.ikine(TL, Q0,'mask', [1 1 0 0 0 1]);
-ThreeLink.plot(QF);
-TRAJ = jtraj(Q0, QF, (0:.05:1));
-ThreeLink.plot(TRAJ);
+ThreeLink.base=[10 10 0];
+t=(0:0.05:2)';
+a = xlsread('GGGLetter.xlsx');
+
+
+
+masd=mstraj(a,[1,2,0],[],a(1,:),0.05,0)
+TRAJ1=ThreeLink.fkine(masd);
+yyt=transl(masd);
+q1=ThreeLink.ikine(transl(masd),[0 0 0],'mask',[1 1 0 0 0 1]);
+
+scatter(a(:,1),a(:,2),'filled','bo');
+ThreeLink.plot(q1,'loop','workspace',[-20 20 -20 20 -20 20])
+
+
+
