85a0aa31ef8ff9611db88d62ad59e71d0fc14a42
Abdulrhman Ghnem <abdalrhman.ib1998@gmail.com>
2021-06-09 21:14:50 +0800

    add more accrate points and adjust the workspace

--- PlanarRobot3DOFSimulation.m
+++ PlanarRobot3DOFSimulation.m
@@ -9,12 +9,12 @@ L(3) = Link([0 0 L3 0]);%D-H parameter for link 1
 ThreeLink = SerialLink(L);% construct the robot 
 ThreeLink.name = 'Planar3R'; % give it a name
 ThreeLink.base=[0 -10 0]; % define the base
-a = xlsread('GHNEM.xlsx'); % read the points from an excel file 
-masd=mstraj(a,[4,4,0],[],a(1,:),0.1,0); % use the mstraj function to find the trajectory 
+a = xlsread('GHNEM1.xlsx'); % read the points from an excel file 
+masd=mstraj(a,[6,6,0],[],a(1,:),0.1,0); % use the mstraj function to find the trajectory 
 trsl=transl(masd); % find the x-y-z of the trajectory 
 q1=ThreeLink.ikine(transl(masd),[0 0 0],'mask',[1 1 0 0 0 1]); % find the inverse kinematics 
-plot3(a(:,1),a(:,2), a(:,3)); % plot the points from an excel file
-ThreeLink.plot(q1,'loop','workspace',[-10 20 -20 20 0 20]); % plot the robot giving it the inverse kinematics 
+scatter(a(:,1),a(:,2),'filled','bo');% plot the points from an excel file
+ThreeLink.plot(q1,'loop','workspace',[-20 20 -20 20 0 20]); % plot the robot giving it the inverse kinematics 
 
 
 
